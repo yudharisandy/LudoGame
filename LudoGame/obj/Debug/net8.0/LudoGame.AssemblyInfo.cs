d8b2f6968dc8211193e96b7f5967d65de058bb63
yudharisandy <yudhapa17@gmail.com>
2024-03-09 00:49:54 +0900

    update image

--- LudoGame/obj/Debug/net8.0/LudoGame.AssemblyInfo.cs
+++ LudoGame/obj/Debug/net8.0/LudoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LudoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be42ef6c6e04bb35ab367fc1dcacd6d7d8d4b10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5bfcd5e0c336975b1840e41d9e337429593a472")]
 [assembly: System.Reflection.AssemblyProductAttribute("LudoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LudoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
